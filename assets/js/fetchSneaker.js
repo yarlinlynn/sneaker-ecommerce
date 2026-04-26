ad3183250e4671deda11bf634180b28e5bd99a85
Yarlin <yarlinlynn@gmail.com>
2026-04-26 19:20:45 +0200

    Changed fetch file path to correctly render sneakers from sneaker.json file when visiting site on github pages

--- assets/js/fetchSneaker.js
+++ assets/js/fetchSneaker.js
@@ -3,15 +3,14 @@
 
 export async function fetchSneakers() {
   try {
-    const response = await fetch('../data/sneakers.json');
+    const response = await fetch('../../data/sneakers.json');
 
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
     return data;
-
 
   } catch (error) {
     console.error('Error fetching data:', error);
