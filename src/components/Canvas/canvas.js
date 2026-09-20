0fddd148db3f71416a1fe6e7487dd72692ed0fda
Yarlin <yarlinlynn@gmail.com>
2026-09-20 09:42:57 +0200

    Replace canvas component with updated component, filtering canvasItems to only render sneaker data to DOM

--- src/components/Canvas/canvas.js
+++ src/components/Canvas/canvas.js
@@ -1,44 +1,30 @@
 
 import { canvasItems } from "./canvasData.js";
 
-// render sneaker data to DOM
+// render sneaker boxes to DOM
 export function Canvas() {
     return `
-        ${canvasItems.map((item) => {
+        ${canvasItems.filter(item => item.type === "sneaker").map(item => `
+            <section class="float-left w-[240px] h-[240px] p-[10px] bg-[#f4efef] border border-[#dadada]">
 
-            if (item.type === "collection") {
-                return `
-                   <section 
-                        class="float-left w-[240px] h-[240px] p-[10px] bg-[#f4efef] border border-[#dadada] flex items-center justify-center text-center hover:bg-white hover:text-[#111]">
-                        <h1 data-type="collection" id="collection-${item.id}" class="text-[26px] font-medium uppercase hover:cursor-pointer">
-                            ${item.name}
-                        </h1>
-                   </section> 
-                `;
-            }
+                <img class="w-full h-[80%] object-contain transition-opacity duration-400 ease-in-out cursor-pointer" 
+                    src="${item.images[0]}" 
+                    data-default="${item.images[0]}" 
+                    data-hover="${item.images[1] || item.images[0]}" 
+                    data-sneaker-id="${item.id}"
+                    data-click="sneaker"
+                    alt="${item.name}" loading="lazy"
+                />
 
-            return `
-                <section data-collection-id="${item.collection}"
-                    class="float-left w-[240px] h-[240px] p-[10px] bg-[#f4efef] border border-[#dadada]">
-                    <img class="w-full h-[80%] object-contain transition-opacity duration-400 ease-in-out cursor-pointer" 
-                        src="${item.images[0]}" 
-                        data-default="${item.images[0]}" 
-                        data-hover="${item.images[1] || item.images[0]}" 
-                        data-sneaker-id="${item.id}"
-                        data-click="sneaker"
-                        alt="${item.name}" loading="lazy"
-                    />
-
-                    <div class="content">
-                        <p class="mt-[-15px] text-[10px] uppercase font-medium text-gray-500"> 
-                            ${item.name} 
-                        </p> 
-                        <h1 data-click="sneaker" data-sneaker-id="${item.id}" class="my-[5px] text-[26px] font-medium cursor-pointer"> 
-                            ${item.color} 
-                        </h1>
-                    </div>
-                </section>
-            `;
-        }).join("")}
+                <div class="content">
+                    <p class="mt-[-15px] text-[10px] uppercase font-medium text-gray-500"> 
+                        ${item.name} 
+                    </p> 
+                    <h1 data-click="sneaker" data-sneaker-id="${item.id}" class="my-[5px] text-[26px] font-medium cursor-pointer"> 
+                        ${item.color} 
+                    </h1>
+                </div>
+            </section>
+        `).join("")}
     `;
 }
