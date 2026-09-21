4c38f15a889cb3c153c07be1d3da37f5aed7cc71
Yarlin <yarlinlynn@gmail.com>
2026-09-21 19:55:50 +0200

    Updated and restyled the modal page component

--- src/components/Modal/productModalPage.js
+++ src/components/Modal/productModalPage.js
@@ -1,47 +1,60 @@
 
+import { Header } from "../header";
+
 export function openProductModalPage(sneaker) {
     return `
         <div class="modal fixed inset-0 z-50 flex h-screen w-screen bg-[#f4efef] opacity-0">
 
-            <div class="relative flex h-full p-8">
-                <button type="button" data-modal-close aria-label="Close product"
-                    class="absolute right-8 top-8 z-20 flex h-10 w-10 items-center justify-center rounded-full bg-black text-white cursor-pointer"
-                >
-                    <span class="text-xl pb-[3px]">
-                        &times;
-                    </span>
-                </button>
+            <div class="container">
+                
+                <div class="modal-header">
+                    ${Header()}
+                    <button type="button" data-modal-close aria-label="Close product"
+                        class="absolute top-8 right-[15rem] z-20 flex h-10 w-10 items-center justify-center rounded-full bg-black text-white cursor-pointer"
+                    >
+                        <span class="text-xl pb-[3px]">&times;</span>
+                    </button>
+                </div>
 
-                <div class="flex flex-5 h-full w-full gap-4">
-                    ${sneaker.images.map((image, index) => `
-                        <div class="flex items-center justify-center">
-                            <img
+                <!-- Modal content below slideshow images and sneaker data eg; name, collection etc -->
+                <div class="">
+                    <div class="flex h-full w-[50%] gap-4">
+                        ${sneaker.images.map((image, index) => `
+                            <img id="${image.id}"
                                 src="${image}"
                                 alt="${sneaker.name} - image ${index + 1}"
                                 loading="lazy"
-                                class="h-full w-full"
                             />
-                        </div>
-                    `).join("")}
-                </div>
-
-                <div class="flex flex-2 h-full flex-col justify-center p-8 z-10 hidden">
-                    <h1 class="text-[40px] font-medium">
-                        ${sneaker.name}
-                    </h1>
-                    <p class="text-sm">
-                        ${sneaker.description || ""}
-                    </p>
-                    <p class="text-sm uppercase text-[#7a7a7a]">
-                        ${sneaker.color}
-                    </p>
-                    <p class="mt-2 text-sm font-medium">
-                        ${sneaker.price}
-                    </p>
-
-                    <div class="suggestion_sneakers"></div>
+                        `).join("")}
+                    </div>
+                    <div class="modal-content px-8 grid grid-cols-2 gap-2">
+                        <h1 class="text-[4rem] font-medium">${sneaker.name}</h1>
+                        <p class="text-[1rem] w-[40rem] col-start-1 col-end-2">${sneaker.description || ""}</p>
+                        <h3 class="text-[1.5rem] uppercase text-[#7a7a7a] ml-auto">${sneaker.color}</h3>
+                        <p class="mt-2 text-[3rem] font-medium col-start-2 row-start-1 ml-auto">R${sneaker.price.toFixed(2)}</p>
+                    </div>
                 </div>
             </div>
+
+            <div class="related-sneaker bg-[#f4efef] border-l border-l-gray-500 ml-4">
+                <div class="box w-[200px] bg-white cursor-pointer">
+                    <img src="https://images.ctfassets.net/hnk2vsx53n6l/3R7XxtWYepzfp59mncflPM/872d5d3beb6e35bd6411d2ec9e3c0990/411e207ef06741c81d79ca0298bae221e2981a4a.png?w=1200&h=1200&fm=avif&f=center&fit=fill&q=80" alt="" loading="lazy" />
+                </div>
+                <div class="box w-[200px] cursor-pointer">
+                    <img src="https://images.ctfassets.net/hnk2vsx53n6l/3R7XxtWYepzfp59mncflPM/872d5d3beb6e35bd6411d2ec9e3c0990/411e207ef06741c81d79ca0298bae221e2981a4a.png?w=1200&h=1200&fm=avif&f=center&fit=fill&q=80" alt="" loading="lazy" />
+                </div>
+                <div class="box w-[200px] cursor-pointer">
+                    <img src="https://images.ctfassets.net/hnk2vsx53n6l/3R7XxtWYepzfp59mncflPM/872d5d3beb6e35bd6411d2ec9e3c0990/411e207ef06741c81d79ca0298bae221e2981a4a.png?w=1200&h=1200&fm=avif&f=center&fit=fill&q=80" alt="" loading="lazy" />
+                </div>
+                <div class="box w-[200px] cursor-pointer">
+                    <img src="https://images.ctfassets.net/hnk2vsx53n6l/3R7XxtWYepzfp59mncflPM/872d5d3beb6e35bd6411d2ec9e3c0990/411e207ef06741c81d79ca0298bae221e2981a4a.png?w=1200&h=1200&fm=avif&f=center&fit=fill&q=80" alt="" loading="lazy" />
+                </div>
+                <div class="box w-[200px] cursor-pointer">
+                    <img src="https://images.ctfassets.net/hnk2vsx53n6l/3R7XxtWYepzfp59mncflPM/872d5d3beb6e35bd6411d2ec9e3c0990/411e207ef06741c81d79ca0298bae221e2981a4a.png?w=1200&h=1200&fm=avif&f=center&fit=fill&q=80" alt="" loading="lazy" />
+                </div>
+
+            </div>
+
         </div>
     `;
 }
